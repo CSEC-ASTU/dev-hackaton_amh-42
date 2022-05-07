2e2ea2130147478694227c6f59a8d06eee59078f
Amh42 <anwarmisbaham@gmail.com>
2022-05-07 13:09:45 +0300

    [add] Database Connector

---
+++ database/DBConnector.php
@@ -0,0 +1,11 @@
+<?php
+function connect_db()
+{
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "csec_astu";
+    $con = mysqli_connect($host, $user, $password, $database);
+    if ($con->connect_error) return "Fail" . $con->connect_error;
+    return "Successfully Connected!";
+}
