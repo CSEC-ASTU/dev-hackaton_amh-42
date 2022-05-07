b7feeb902139ccf59b6e0400e5bb69efa098b85a
Amh42 <anwarmisbaham@gmail.com>
2022-05-07 15:27:23 +0300

    [edit] admin dashbord and main page link

--- verification.php
+++ verification.php
@@ -1,9 +1,20 @@
 
 <?php
 include('database/DBConnector.php');
-$connection = connect_db();
-echo $connection;
+$con = connect_db();
 
 $email = $_POST["email"];
 $pass = $_POST["pass"];
-$sql = "SELECT password FROM user WHERE email = '$email'";
+$sql = "SELECT password, level FROM user WHERE email = '$email'";
+$result = mysqli_query($con, $sql);
+$resultArray = array();
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$new_pass = $row['password'];
+$level = $row['level'];
+if ($new_pass == $pass) {
+    if ($level == 'admin') {
+        echo 'Successfully Logged in <br><a href="Admin.php">Admin Dashboard</a><br><a href="scoreboard.php">Main Page</a>';
+    } else {
+        echo 'Successfully Logged in <a href="Scoreboard.php">Click Here</a>';
+    }
+} else echo "Wrong Password";
