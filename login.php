2a60f7beed8c135c5ddf6f7915341054f6c437f4
Amh42 <anwarmisbaham@gmail.com>
2022-05-07 13:10:49 +0300

    [add] login page

---
+++ login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>CSEC_ASTU | Log in</title>
+
+
+  <!-- Font Awesome -->
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins\fontawesome-free\css\all.min.css" />
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="plugins\icheck-bootstrap\icheck-bootstrap.min.css" />
+  <!-- Theme style -->
+  <link rel="stylesheet" href="plugins\dist\css\adminlte.min.css" />
+
+<body class="hold-transition login-page">
+  <div class="login-box">
+    <div class="login-logo">
+      <a href="../../index2.html"><b>CSEC_ASTU</b></a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="card">
+      <div class="card-body login-card-body">
+        <p class="login-box-msg">Sign in to start your session</p>
+
+        <form action="verification.php" method="post">
+          <div class="input-group mb-3">
+            <input type="email" class="form-control" placeholder="Email" name="email" />
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Password" name="pass" />
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-4">
+              <button type="submit" class="btn btn-primary btn-block">
+                Sign In
+              </button>
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+
+
+        <p class="mb-0">
+          <a href="register.php" class="text-center">Register a new membership</a>
+        </p>
+      </div>
+      <!-- /.login-card-body -->
+    </div>
+  </div>
+  <!-- /.login-box -->
+
+</body>
+
+</html>
