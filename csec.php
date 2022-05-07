53455e1d910d177d070522a9878373274c4b34b7
Amh42 <anwarmisbaham@gmail.com>
2022-05-07 13:09:18 +0300

    [add] landing page

---
+++ csec.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="custom.css">
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css" />
+    <link rel="stylesheet" href="plugins/dist/css/adminlte.min.css" />
+    <title>CSEC_ASTU</title>
+</head>
+
+<body class="welcome">
+    <div class="text">
+        <form action="login.php">
+            <p>Welcome to CSEC</p>
+            <button type="submit" class="btn btn-primary">Get Started</button>
+        </form>
+    </div>
+</body>
+
+</html>
