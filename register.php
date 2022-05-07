9349b7f4dd92ab99e99037880b75d1a4d8944783
Amh42 <anwarmisbaham@gmail.com>
2022-05-07 13:11:27 +0300

    [add] registration page

---
+++ register.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>CSEC_ASTU | Registration Page</title>
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins\fontawesome-free\css\all.min.css" />
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="plugins\icheck-bootstrap\icheck-bootstrap.min.css" />
+  <!-- Theme style -->
+  <link rel="stylesheet" href="plugins\dist\css\adminlte.min.css" />
+</head>
+
+<body class="hold-transition register-page">
+  <div class="register-box">
+    <div class="register-logo">
+      <a href="../../index2.html"><b>CSEC_ASTU</b></a>
+    </div>
+
+    <div class="card">
+      <div class="card-body register-card-body">
+        <p class="login-box-msg">Register a new membership</p>
+
+        <form action="scoreboard.php" method="post">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Full name" />
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="email" class="form-control" placeholder="Email" />
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Password" />
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" type="checkbox" id="customCheckbox1" value="option1">
+            <label for="customCheckbox1" class="custom-control-label">Dev</label>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" type="checkbox" id="customCheckbox2" value="option1">
+            <label for="customCheckbox2" class="custom-control-label">CPD</label>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" type="checkbox" id="customCheckbox3" value="option1">
+            <label for="customCheckbox3" class="custom-control-label">CBD</label>
+          </div>
+          <div class="row">
+            <div class="col-4">
+              <button type="submit" class="btn btn-primary btn-block">
+                Register
+              </button>
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+        <a href="login.php" class="text-center">I already have a membership</a>
+      </div>
+      <!-- /.form-box -->
+    </div>
+    <!-- /.card -->
+  </div>
+  <!-- /.register-box -->
+</body>
+
+</html>
