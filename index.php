5cec3eed7d829a654a47497248e6b0843a174a1b
Amh42 <anwarmisbaham@gmail.com>
2022-05-08 07:38:47 +0300

    [add] landing page

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Add Events</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="plugins/CSS/w3.css" />
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css" />
+    <link rel="stylesheet" href="plugins/dist/css/adminlte.min.css" />
+    <link rel="stylesheet" href="custom.css">
+</head>
+
+<body>
+    <div class="w3-top">
+        <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+            <a href="#home" class="w3-bar-item w3-button"><b>CSEC</b> ASTU</a>
+            <!-- Float links to the right. Hide them on small screens -->
+            <div class="w3-right w3-hide-small">
+                <a href="#projects" class="w3-bar-item w3-button">CPD</a>
+                <a href="#about" class="w3-bar-item w3-button">Dev</a>
+                <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+            </div>
+        </div>
+    </div>
+    <!-- Add Event -->
+    <img src="w3images/Landing.png" alt="">
+    <div class="welcome">
+        <div class="text">
+            <form action="login.php">
+                <p>Welcome to CSEC</p>
+                <button type="submit" class="btn btn-primary">Get Started</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
